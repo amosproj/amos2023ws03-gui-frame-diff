d716dad6980ccb153b062d2dd4cfdef2f4cd01c4
a-miscellaneous <96189996+a-miscellaneous@users.noreply.github.com>
2023-11-10 18:41:55 +0100

    basic setup
    Signed-off-by: a-miscellaneous <96189996+a-miscellaneous@users.noreply.github.com>

--- DifferenceGenerator/src/main/kotlin/DifferenceGenerator.kt
+++ DifferenceGenerator/src/main/kotlin/DifferenceGenerator.kt
@@ -1,4 +1,17 @@
+import org.bytedeco.ffmpeg.global.avcodec.AV_CODEC_ID_FFV1
+
 import org.bytedeco.javacv.FFmpegFrameGrabber
+import org.bytedeco.javacv.FFmpegFrameRecorder
+import org.bytedeco.javacv.Frame
+import org.bytedeco.javacv.OpenCVFrameConverter.ToMat
+import org.bytedeco.opencv.global.opencv_core.CV_8UC3
+import org.bytedeco.opencv.global.opencv_core.absdiff
+
+import org.bytedeco.opencv.opencv_core.*
+import org.bytedeco.opencv.opencv_imgproc.*
+import org.bytedeco.opencv.opencv_core.Mat
+
+
 import java.io.File
 
 class DifferenceGenerator(video1Path: String, video2Path: String, outputPath: String) :
@@ -33,8 +46,8 @@ class DifferenceGenerator(video1Path: String, video2Path: String, outputPath: St
     private fun isLosslessCodec(grabber: FFmpegFrameGrabber): Boolean {
         grabber.start()
         val codecName = grabber.videoMetadata["encoder"]
-        grabber.stop()
         if (codecName == null) {
+            grabber.stop()
             throw Exception("Video must have a codec")
         }
 
@@ -42,8 +55,46 @@ class DifferenceGenerator(video1Path: String, video2Path: String, outputPath: St
     }
 
     override fun generateDifference() {
-        println("Generating difference ")
+        val encoder = FFmpegFrameRecorder(this.outputFile, video1Grabber.imageWidth, video1Grabber.imageHeight)
+        encoder.videoCodec = AV_CODEC_ID_FFV1
+        encoder.start()
+
+        var frame1 = this.video1Grabber.grabImage()
+        var frame2 = this.video2Grabber.grabImage()
+
+        while (frame1 != null && frame2 != null) {
+            val differences = getDifferences(frame1, frame2)
+            encoder.record(differences)
+            frame1 = this.video1Grabber.grabImage()
+            frame2 = this.video2Grabber.grabImage()
+        }
+
+        encoder.stop()
+        encoder.release()
+        this.video1Grabber.stop()
+        this.video2Grabber.stop()
     }
+
+
+    private fun getDifferences(frame1: Frame, frame2: Frame): Frame {
+        val width = frame1.imageWidth
+        val height = frame1.imageHeight
+        val differences = Frame(width, height, frame1.imageDepth, frame1.imageChannels)
+
+
+        val frame1Pixels = frame1.image[0]
+        val frame2Pixels = frame2.image[0]
+
+
+        val capacity = frame1Pixels.capacity()
+
+
+
+        return differences
+
+    }
+
+
 
     override fun saveDifferences() {}
 }
