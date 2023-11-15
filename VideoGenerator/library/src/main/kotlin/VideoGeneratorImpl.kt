4bfb5d37c19f7a76e6a4395ea0730ff4e36a0728
zino212 <luisguenther@gmx.de>
2023-11-15 08:29:38 +0100

    feat(lib1): Added first implementation of processing
    Signed-off-by: zino212 <luisguenther@gmx.de>

--- VideoGenerator/library/src/main/kotlin/VideoGeneratorImpl.kt
+++ VideoGenerator/library/src/main/kotlin/VideoGeneratorImpl.kt
@@ -1,6 +1,9 @@
-
-import java.util.LinkedList
-import java.util.Queue
+import org.bytedeco.ffmpeg.global.avcodec
+import org.bytedeco.javacv.FFmpegFrameRecorder
+import org.bytedeco.javacv.Java2DFrameConverter
+import java.io.ByteArrayInputStream
+import java.util.*
+import javax.imageio.ImageIO
 
 class VideoGeneratorImpl(private val videoPath: String, private val imageWidth: Int, private val imageHeight: Int) : AbstractVideoGenerator(
     videoPath,
@@ -13,7 +16,7 @@ class VideoGeneratorImpl(private val videoPath: String, private val imageWidth: 
      * Here, you can initialize your complex member object and
      * make sure that the instance is prepared for incoming frames.
      */
-    init { }
+    init {}
 
     /**
      * Appends the image bytes to the internal queue for further processing.
@@ -22,7 +25,36 @@ class VideoGeneratorImpl(private val videoPath: String, private val imageWidth: 
         queue.add(frameBytes)
     }
 
-    override fun save() {
-        // To be implemented
+    /**
+     * Process frames from a queue and record them into a video file.
+     */
+    override fun processFrames() {
+        val recorder: FFmpegFrameRecorder =
+            FFmpegFrameRecorder(videoPath, imageWidth, imageHeight).apply {
+                videoCodec = avcodec.AV_CODEC_ID_FFV1
+                format = "matroska"
+                frameRate = 25.0
+            }
+
+        val converter = Java2DFrameConverter()
+
+        try {
+            recorder.start()
+            while (queue.isNotEmpty()) {
+                val image = queue.poll()
+                ByteArrayInputStream(image).use { bis ->
+                    val bImage = ImageIO.read(bis)
+                    val frame = converter.convert(bImage)
+                    recorder.record(frame)
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            recorder.stop()
+            recorder.release()
+        }
     }
+
+    override fun save() {}
 }
