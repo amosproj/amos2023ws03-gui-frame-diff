08b407e02e31f5f4563cb922bc6bb04f7609f001
Anton Kriese <anton.kriese@fu-berlin.de>
2023-11-20 19:46:33 +0100

    feat(lib2): example with strings for Gotoh alg.
    Co-authored-by: Simon Sasse <simonsasse97@gmail.com>
    Signed-off-by: Anton Kriese <anton.kriese@fu-berlin.de>

--- DifferenceGenerator/src/main/kotlin/Main.kt
+++ DifferenceGenerator/src/main/kotlin/Main.kt
@@ -1,4 +1,48 @@
+import algorithms.Gotoh
+
 fun main(args: Array<String>) {
     println("Hello World!")
+
     println("Program arguments: ${args.joinToString()}")
+    val metric: DnaMetric = DnaMetric()
+    val algorithm: AlignmentAlgorithm<Char> = Gotoh(metric, -10.0, -1.0)
+
+    val a: Array<Char> = arrayOf('A', 'A', 'A', 'A', 'G', 'G', 'T', 'A', 'C', 'G', 'C', 'A', 'C', 'G', 'T')
+    val b: Array<Char> = arrayOf('A', 'A', 'A', 'A', 'G', 'G', 'T', 'A', 'C', 'G', 'C', 'A', 'C', 'G', 'T')
+
+    val alignment: Array<AlignmentElement> = algorithm.run(a, b)
+
+    var l1 = ArrayList<Char>()
+    var l2 = ArrayList<Char>()
+    var lDifference = ArrayList<Char>()
+
+    var i = 0
+    var j = 0
+
+    for (al in alignment) {
+        when (al) {
+            AlignmentElement.MATCH -> {
+                l1.add(a[i])
+                l2.add(b[j])
+                lDifference.add('|')
+                i++
+                j++
+            }
+            AlignmentElement.INSERTION -> {
+                l1.add('-')
+                l2.add(b[j])
+                lDifference.add(' ')
+                j++
+            }
+            AlignmentElement.DELETION -> {
+                l1.add(a[i])
+                l2.add('-')
+                lDifference.add(' ')
+                i++
+            }
+        }
+    }
+    println(l1.joinToString())
+    println(lDifference.joinToString())
+    println(l2.joinToString())
 }
