46bbfecf7468b809fa9f15ea9edd1b91a6c825c1
zino212 <luisguenther@gmx.de>
2023-11-20 11:04:04 +0100

    refactor(lib1): Rewrite generator implementation to have only two functions and fill init method
    Signed-off-by: zino212 <luisguenther@gmx.de>

--- VideoGenerator/library/src/main/kotlin/VideoGeneratorImpl.kt
+++ VideoGenerator/library/src/main/kotlin/VideoGeneratorImpl.kt
@@ -1,70 +1,70 @@
 import org.bytedeco.ffmpeg.global.avcodec
 import org.bytedeco.javacv.FFmpegFrameRecorder
 import org.bytedeco.javacv.Java2DFrameConverter
+import java.awt.image.BufferedImage
 import java.io.ByteArrayInputStream
-import java.util.*
 import javax.imageio.ImageIO
 
-class VideoGeneratorImpl(private val videoPath: String, private val imageWidth: Int, private val imageHeight: Int) : AbstractVideoGenerator(
-    videoPath,
-    imageWidth,
-    imageHeight,
-) {
-    private val queue: Queue<ByteArray> = LinkedList()
+/**
+ * A class that implements the AbstractVideoGenerator interface to generate video files.
+ *
+ * @param videoPath The path to the output video file.
+ * @param imageWidth The width of each frame in the video.
+ * @param imageHeight The height of each frame in the video.
+ */
+class VideoGeneratorImpl(
+    private val videoPath: String,
+    private val imageWidth: Int,
+    private val imageHeight: Int,
+) : AbstractVideoGenerator(videoPath, imageWidth, imageHeight) {
 
-    /**
-     * Here, you can initialize your complex member object and
-     * make sure that the instance is prepared for incoming frames.
-     */
-    init {}
+    private val recorder: FFmpegFrameRecorder = initializeRecorder()
+    private val converter = Java2DFrameConverter()
 
-    /**
-     * Appends the image bytes to the internal queue for further processing.
-     */
-    override fun loadFrame(frameBytes: ByteArray) {
-        queue.add(frameBytes)
+    init {
+        recorder.start()
     }
 
     /**
-     * Process frames from a queue and record them into a video file.
+     * Appends an image to the video file.
      *
-     * @throws Exception if any error occurs during the processing.
+     * @param frameBytes the byte array containing the image data
      */
-    override fun processFrames() {
-        // FFmpegFrameRecorder is created (class provided by JavaCV library)
-        val recorder: FFmpegFrameRecorder =
-            FFmpegFrameRecorder(videoPath, imageWidth, imageHeight).apply {
-                videoCodec = avcodec.AV_CODEC_ID_FFV1
-                format = "matroska"
-                frameRate = 25.0
+    override fun loadFrame(frameBytes: ByteArray) {
+        ByteArrayInputStream(frameBytes).use { bis ->
+            val bImage = ImageIO.read(bis)
+            bImage?.let {
+                recordFrame(it)
             }
-
-        // Java2DFrameConverter is created (allowing conversion between Java's
-        // native image data structure (BufferedImage) and JavaCV's Frame object)
-        val converter = Java2DFrameConverter()
-
-        try {
-            recorder.start()
-            while (queue.isNotEmpty()) {
-                // fetch image from the queue
-                val image = queue.poll()
-                ByteArrayInputStream(image).use { bis ->
-                    // 'image' bytes are read and converted to BufferedImage object 'bImage'
-                    val bImage = ImageIO.read(bis)
-                    // BufferedImage object is converted to Frame object
-                    val frame = converter.convert(bImage)
-                    // Frame object is recorded into video file
-                    recorder.record(frame)
-                }
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            // video recording is stopped and the resources are released
-            recorder.stop()
-            recorder.release()
         }
     }
 
-    override fun save() {}
+    /**
+     * This method stops the ongoing recording and releases the resources used by the recorder.
+     */
+    override fun save() {
+        recorder.stop()
+        recorder.release()
+    }
+
+    /**
+     * Initializes the recorder with the specified video path, image width, and image height.
+     *
+     * @return an instance of FFmpegFrameRecorder initialized with the specified parameters.
+     */
+    private fun initializeRecorder() = FFmpegFrameRecorder(videoPath, imageWidth, imageHeight).apply {
+        videoCodec = avcodec.AV_CODEC_ID_FFV1
+        format = "matroska"
+        frameRate = 25.0
+    }
+
+    /**
+     * Records a frame.
+     *
+     * @param bImage the BufferedImage representing the frame to be recorded
+     */
+    private fun recordFrame(bImage: BufferedImage) {
+        val frame = converter.convert(bImage)
+        recorder.record(frame)
+    }
 }
--- VideoGenerator/library/src/main/kotlin/VideoGeneratorImplTests.kt
+++ VideoGenerator/library/src/main/kotlin/VideoGeneratorImplTests.kt
@@ -1,5 +1,5 @@
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.io.File
@@ -9,12 +9,28 @@ import java.nio.file.Paths
 class VideoGeneratorImplTest {
     private lateinit var videoGenerator: VideoGeneratorImpl
     private val videoPath = "src/resources/testOutput.mkv"
-    private lateinit var exampleImageData: ByteArray
+    private lateinit var exampleImageData1: ByteArray
+    private lateinit var exampleImageData2: ByteArray
+    private lateinit var exampleImageData3: ByteArray
+    private lateinit var exampleImageData4: ByteArray
+    private lateinit var exampleImageData5: ByteArray
+    private lateinit var exampleImageData6: ByteArray
+    private lateinit var exampleImageData7: ByteArray
+    private lateinit var exampleImageData8: ByteArray
+    private lateinit var exampleImageData9: ByteArray
 
     @BeforeEach
     fun setUp() {
         videoGenerator = VideoGeneratorImpl(videoPath, 640, 480)
-        exampleImageData = Files.readAllBytes(Paths.get("src/resources/Screenshot_50.png"))
+        exampleImageData1 = Files.readAllBytes(Paths.get("src/resources/Screenshot_50.png"))
+        exampleImageData2 = Files.readAllBytes(Paths.get("src/resources/Screenshot_51.png"))
+        exampleImageData3 = Files.readAllBytes(Paths.get("src/resources/Screenshot_52.png"))
+        exampleImageData4 = Files.readAllBytes(Paths.get("src/resources/Screenshot_50.png"))
+        exampleImageData5 = Files.readAllBytes(Paths.get("src/resources/Screenshot_51.png"))
+        exampleImageData6 = Files.readAllBytes(Paths.get("src/resources/Screenshot_52.png"))
+        exampleImageData7 = Files.readAllBytes(Paths.get("src/resources/Screenshot_50.png"))
+        exampleImageData8 = Files.readAllBytes(Paths.get("src/resources/Screenshot_51.png"))
+        exampleImageData9 = Files.readAllBytes(Paths.get("src/resources/Screenshot_52.png"))
     }
 
     @AfterEach
@@ -23,9 +39,17 @@ class VideoGeneratorImplTest {
     }
 
     @Test
-    fun testLoadFrameAndProcessFrames() {
-        videoGenerator.loadFrame(exampleImageData)
-        videoGenerator.processFrames()
+    fun testLoadFrameAndSave() {
+        videoGenerator.loadFrame(exampleImageData1)
+        videoGenerator.loadFrame(exampleImageData2)
+        videoGenerator.loadFrame(exampleImageData3)
+        videoGenerator.loadFrame(exampleImageData4)
+        videoGenerator.loadFrame(exampleImageData5)
+        videoGenerator.loadFrame(exampleImageData6)
+        videoGenerator.loadFrame(exampleImageData7)
+        videoGenerator.loadFrame(exampleImageData8)
+        videoGenerator.loadFrame(exampleImageData9)
+        videoGenerator.save()
 
         assertTrue(Files.exists(Paths.get(videoPath)))
     }
