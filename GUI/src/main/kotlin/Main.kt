1bab4f338f5221a47d7ccb83cf51dc32b53717d3
Luis Günther <52202194+zino212@users.noreply.github.com>
2023-11-27 18:05:02 +0100

    feat(gui): Added file dialogues and documentation
    Signed-off-by: zino212 <luisguenther@gmx.de>

--- GUI/src/main/kotlin/Main.kt
+++ GUI/src/main/kotlin/Main.kt
@@ -1,32 +1,55 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+import androidx.compose.runtime.*
+import androidx.compose.ui.window.*
+import java.io.File
+import javax.swing.JFileChooser
 
+/**
+ * The App composable function is the main entry point for the application.
+ * It renders a UI that allows the user to select two videos and display them using VideoPlayer.
+ *
+ * @return The composable function does not return anything.
+ */
 @Composable
-@Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    var videoFile1 by remember { mutableStateOf<File?>(null) }
+    var videoFile2 by remember { mutableStateOf<File?>(null) }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column {
+            Button(onClick = { videoFile1 = selectVideo() }) {
+                Text("Select video 1")
+            }
+
+            Button(onClick = { videoFile2 = selectVideo() }) {
+                Text("Select video 2")
+            }
         }
     }
 }
 
-fun main() =
-    application {
-        Window(onCloseRequest = ::exitApplication) {
-            App()
-        }
+/**
+ * The main function starts the application and opens a window. It sets the onCloseRequest event to
+ * exit the application and initializes the App.
+ */
+fun main(): Unit = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
     }
+}
+
+/**
+ * Prompts the user to select a video file using a file chooser dialog.
+ *
+ * @return The selected video file, or null if no file was selected.
+ */
+fun selectVideo(): File? {
+    val chooser = JFileChooser()
+    return if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+        chooser.selectedFile
+    } else {
+        null
+    }
+}
