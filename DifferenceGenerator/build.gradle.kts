489bd9c656cb69407eb6b0f0ea4ba6ad3c01db40
Anton Kriese <anton.kriese@fu-berlin.de>
2023-11-27 16:11:53 +0100

    feat(license): use gradle plugin to perform license check
    This uses the gradle plugin available at https://github.com/jk1/Gradle-License-Report.
    So far only tested for lib2 (DifferenceGenerator)
    
    Signed-off-by: Anton Kriese <anton.kriese@fu-berlin.de>

--- DifferenceGenerator/build.gradle.kts
+++ DifferenceGenerator/build.gradle.kts
@@ -1,6 +1,12 @@
+import com.github.jk1.license.filter.DependencyFilter
+import com.github.jk1.license.filter.LicenseBundleNormalizer
+import com.github.jk1.license.render.InventoryHtmlReportRenderer
+import com.github.jk1.license.render.ReportRenderer
+
 plugins {
     kotlin("jvm") version "1.8.0"
     application
+    id("com.github.jk1.dependency-license-report") version "2.5"
 }
 
 group = "org.example"
@@ -25,4 +31,11 @@ kotlin {
 
 application {
     mainClass.set("MainKt")
+}
+
+licenseReport {
+    outputDir = "../licenses/reports/DifferenceGenerator"
+    renderers = arrayOf<ReportRenderer>(InventoryHtmlReportRenderer("report.html", "DifferenceGenerator"))
+    filters = arrayOf<DependencyFilter>(LicenseBundleNormalizer())
+    allowedLicensesFile = File("../licenses/allowed-licenses.json")
 }
