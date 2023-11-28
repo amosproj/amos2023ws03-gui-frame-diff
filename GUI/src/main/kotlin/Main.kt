7cda757d019b0b4ea6c3f36c6ece89d1b7fb4438
LuisGuenther <luis.guenther@maibornwolff.de>
2023-11-28 14:41:33 +0100

    feat(gui): Added buttons for playing videos and error handling
    Signed-off-by: zino212 <luisguenther@gmx.de>

--- GUI/src/main/kotlin/Main.kt
+++ GUI/src/main/kotlin/Main.kt
@@ -13,63 +13,7 @@ import javax.swing.*
 import javax.swing.JFileChooser
 
 /**
- * The App composable function is the main entry point for the application.
- * It renders a UI that allows the user to select two videos and display them using VideoPlayer.
- *
- * @return This composable function does not return anything.
- */
-@Composable
-fun App() {
-    var videoPath1 by remember { mutableStateOf<String?>(null) }
-    var videoPath2 by remember { mutableStateOf<String?>(null) }
-
-    Row(Modifier.fillMaxSize()) {
-        videoPanel(videoPath1, onUpdate = { videoPath1 = it }, Modifier.weight(1f))
-        videoPanel(videoPath2, onUpdate = { videoPath2 = it }, Modifier.weight(1f))
-    }
-}
-
-/**
- * Displays a panel that includes a video player and a file selector button.
- *
- * @param videoPath The path of the video file to be played. If null, an empty panel will be displayed.
- * @param onUpdate Callback function that will be called when a new video path is selected.
- * @param modifier The modifier for the video panel.
- */
-@Composable
-fun videoPanel(
-    videoPath: String?,
-    onUpdate: (String) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    Column(modifier.fillMaxSize().padding(16.dp)) {
-        SwingPanel(
-            modifier = Modifier.weight(4f).fillMaxWidth(),
-            factory = {
-                if (videoPath == null) {
-                    JPanel()
-                } else {
-                    JPanel().apply {
-                        layout = BorderLayout()
-                        val mediaPlayerComponent = EmbeddedMediaPlayerComponent()
-                        add(mediaPlayerComponent, BorderLayout.CENTER)
-                        SwingUtilities.invokeLater {
-                            mediaPlayerComponent.mediaPlayer().media().play(videoPath)
-                        }
-                    }
-                }
-            },
-            update = { /* Optional update code here */ },
-        )
-        Spacer(Modifier.height(8.dp))
-        FileSelectorButton("Select Video", onUpdateResult = onUpdate, Modifier.weight(1f).fillMaxWidth())
-    }
-}
-
-/**
- * The main function starts the application and opens a window.
- *
- * @return Unit indicating that the function does not return a value.
+ * The main method that starts the application.
  */
 fun main(): Unit = application {
     Window(onCloseRequest = ::exitApplication) {
@@ -78,11 +22,116 @@ fun main(): Unit = application {
 }
 
 /**
- * Creates a button to select a file.
+ * Composable function that represents the main application screen.
+ */
+@Composable
+fun App() {
+    var videoPath1 by remember { mutableStateOf<String?>(null) }
+    var videoPath2 by remember { mutableStateOf<String?>(null) }
+    val mediaPlayerComponent1 = remember { mutableStateOf<EmbeddedMediaPlayerComponent?>(null) }
+    val mediaPlayerComponent2 = remember { mutableStateOf<EmbeddedMediaPlayerComponent?>(null) }
+
+    Column(Modifier.fillMaxSize()) {
+        Row(Modifier.weight(1f).fillMaxWidth()) {
+            Column(Modifier.weight(1f)) {
+                videoPanel(videoPath1) { path ->
+                    videoPath1 = path
+                    mediaPlayerComponent1.value?.mediaPlayer()?.media()?.prepare(path)
+                }
+            }
+            Column(Modifier.weight(1f)) {
+                videoPanel(videoPath2) { path ->
+                    videoPath2 = path
+                    mediaPlayerComponent2.value?.mediaPlayer()?.media()?.prepare(path)
+                }
+            }
+        }
+
+        Row(
+            Modifier.fillMaxWidth().padding(8.dp),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            Button(onClick = {
+                try {
+                    mediaPlayerComponent1.value?.mediaPlayer()?.controls()?.play()
+                    mediaPlayerComponent2.value?.mediaPlayer()?.controls()?.play()
+                } catch (e: Exception) {
+                    println("Error playing videos: ${e.localizedMessage}")
+                }
+            }) {
+                Text("Play Videos")
+            }
+            Spacer(Modifier.width(4.dp))
+            Button(onClick = {
+                try {
+                    mediaPlayerComponent1.value?.mediaPlayer()?.controls()?.stop()
+                    mediaPlayerComponent2.value?.mediaPlayer()?.controls()?.stop()
+                } catch (e: Exception) {
+                    println("Error stopping videos: ${e.localizedMessage}")
+                }
+            }) {
+                Text("Stop Videos")
+            }
+        }
+    }
+}
+
+/**
+ * Composable function to display a video panel.
+ *
+ * @param videoPath The path to the video file.
+ * @param onFileSelected Callback function to handle file selection.
+ */
+@Composable
+fun videoPanel(
+    videoPath: String?,
+    onFileSelected: (String) -> Unit,
+) {
+    val mediaPlayerComponent = remember { mutableStateOf<EmbeddedMediaPlayerComponent?>(null) }
+
+    Column(Modifier.fillMaxWidth().padding(8.dp)) {
+        SwingPanel(
+            modifier = Modifier.weight(1f).fillMaxWidth(),
+            factory = {
+                when (videoPath) {
+                    null -> JPanel()
+                    else -> JPanel().apply {
+                        layout = BorderLayout()
+                        val mediaPlayer = EmbeddedMediaPlayerComponent()
+                        try {
+                            add(mediaPlayer, BorderLayout.CENTER)
+                            // Initialize the media player with the video
+                            mediaPlayer.mediaPlayer()?.media()?.prepare(videoPath)
+                            mediaPlayerComponent.value = mediaPlayer
+                        } catch (e: Exception) {
+                            println("Error preparing media: ${e.localizedMessage}")
+                        }
+                    }
+                }
+            },
+            update = { panel ->
+                // Update code to update the media player component
+                try {
+                    mediaPlayerComponent.value?.let {
+                        panel.removeAll()
+                        panel.add(it, BorderLayout.CENTER)
+                        panel.revalidate()
+                    }
+                } catch (e: Exception) {
+                    println("Error updating media component: ${e.localizedMessage}")
+                }
+            },
+        )
+        Spacer(Modifier.height(8.dp))
+        FileSelectorButton("Select Video", onFileSelected)
+    }
+}
+
+/**
+ * A Composable function that creates a button with a file selector functionality.
  *
  * @param buttonText The text to be displayed on the button.
- * @param onUpdateResult The callback function to be invoked when a file is selected.
- *                       The selected file is passed as a String parameter to the function.
+ * @param onUpdateResult A function that will be called with the selected file path as a parameter.
  * @param modifier Optional modifier for the button.
  */
 @Composable
@@ -92,21 +141,26 @@ fun FileSelectorButton(
     modifier: Modifier = Modifier,
 ) {
     Button(onClick = {
-        val path = openFileChooserAndGetPath()
-        if (path != null) {
-            onUpdateResult(path)
+        try {
+            val path = openFileChooserAndGetPath()
+            if (path != null) {
+                onUpdateResult(path)
+            }
+        } catch (e: Exception) {
+            println("Error updating result: ${e.localizedMessage}")
         }
     }, modifier) {
         Text(buttonText)
     }
 }
 
+/**
+ * Opens a file chooser dialog and returns the selected file path.
+ *
+ * @return The selected file path, or null if no file was selected.
+ */
 fun openFileChooserAndGetPath(): String? {
     val fileChooser = JFileChooser()
     val result = fileChooser.showOpenDialog(null)
-    return if (result == JFileChooser.APPROVE_OPTION) {
-        fileChooser.selectedFile.absolutePath
-    } else {
-        null
-    }
+    return if (result == JFileChooser.APPROVE_OPTION) fileChooser.selectedFile.absolutePath else null
 }
