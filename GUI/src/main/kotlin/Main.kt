c0b5b6c89f2aac482fac28793767e8505df62bf1
Luis Günther <52202194+zino212@users.noreply.github.com>
2023-11-28 13:33:09 +0100

    feat(gui): Added opening and showing videos
    Signed-off-by: zino212 <luisguenther@gmx.de>

--- GUI/src/main/kotlin/Main.kt
+++ GUI/src/main/kotlin/Main.kt
@@ -2,10 +2,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.SwingPanel
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
-import java.io.File
+import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent
+import java.awt.BorderLayout
+import javax.swing.*
 import javax.swing.JFileChooser
 
 /**
@@ -16,28 +20,56 @@ import javax.swing.JFileChooser
  */
 @Composable
 fun App() {
-    var videoFile1 by remember { mutableStateOf<File?>(null) }
-    var videoFile2 by remember { mutableStateOf<File?>(null) }
+    var videoPath1 by remember { mutableStateOf<String?>(null) }
+    var videoPath2 by remember { mutableStateOf<String?>(null) }
 
     Row(Modifier.fillMaxSize()) {
-        Column(Modifier.weight(1f).padding(16.dp)) {
-            InfoPanel("Video File 1", videoFile1)
-            Spacer(Modifier.height(8.dp))
-            FileSelectorButton("Video 1") { videoFile1 = it }
-        }
+        videoPanel(videoPath1, onUpdate = { videoPath1 = it }, Modifier.weight(1f))
+        videoPanel(videoPath2, onUpdate = { videoPath2 = it }, Modifier.weight(1f))
+    }
+}
 
-        Column(Modifier.weight(1f).padding(16.dp)) {
-            InfoPanel("Video File 2", videoFile2)
-            Spacer(Modifier.height(8.dp))
-            FileSelectorButton("Video 2") { videoFile2 = it }
-        }
+/**
+ * Displays a panel that includes a video player and a file selector button.
+ *
+ * @param videoPath The path of the video file to be played. If null, an empty panel will be displayed.
+ * @param onUpdate Callback function that will be called when a new video path is selected.
+ * @param modifier The modifier for the video panel.
+ */
+@Composable
+fun videoPanel(
+    videoPath: String?,
+    onUpdate: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier.fillMaxSize().padding(16.dp)) {
+        SwingPanel(
+            modifier = Modifier.weight(4f).fillMaxWidth(),
+            factory = {
+                if (videoPath == null) {
+                    JPanel()
+                } else {
+                    JPanel().apply {
+                        layout = BorderLayout()
+                        val mediaPlayerComponent = EmbeddedMediaPlayerComponent()
+                        add(mediaPlayerComponent, BorderLayout.CENTER)
+                        SwingUtilities.invokeLater {
+                            mediaPlayerComponent.mediaPlayer().media().play(videoPath)
+                        }
+                    }
+                }
+            },
+            update = { /* Optional update code here */ },
+        )
+        Spacer(Modifier.height(8.dp))
+        FileSelectorButton("Select Video", onUpdateResult = onUpdate, Modifier.weight(1f).fillMaxWidth())
     }
 }
 
 /**
  * The main function starts the application and opens a window.
  *
- * @return Unit
+ * @return Unit indicating that the function does not return a value.
  */
 fun main(): Unit = application {
     Window(onCloseRequest = ::exitApplication) {
@@ -48,38 +80,32 @@ fun main(): Unit = application {
 /**
  * Creates a button to select a file.
  *
- * @param name The name or description of the file to be selected.
- * @param onFileSelected The callback function to be invoked when a file is selected.
+ * @param buttonText The text to be displayed on the button.
+ * @param onUpdateResult The callback function to be invoked when a file is selected.
+ *                       The selected file is passed as a String parameter to the function.
+ * @param modifier Optional modifier for the button.
  */
 @Composable
-fun FileSelectorButton(name: String, onFileSelected: (File?) -> Unit) {
-    Button(onClick = { onFileSelected(selectVideo()) }, modifier = Modifier.fillMaxWidth()) {
-        Text("Select $name")
+fun FileSelectorButton(
+    buttonText: String,
+    onUpdateResult: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Button(onClick = {
+        val path = openFileChooserAndGetPath()
+        if (path != null) {
+            onUpdateResult(path)
+        }
+    }, modifier) {
+        Text(buttonText)
     }
 }
 
-/**
- * Composable function to display an info panel with the given name and file.
- *
- * @param name The name to be displayed in the info panel.
- * @param file The file to be displayed in the info panel. Can be null if no file is selected.
- */
-@Composable
-fun InfoPanel(name: String, file: File?) {
-    Card(modifier = Modifier.fillMaxWidth(), elevation = 4.dp) {
-        Text(text = "$name: ${file?.path ?: "No file selected"}", modifier = Modifier.padding(16.dp))
-    }
-}
-
-/**
- * Prompts the user to select a video file using a file chooser dialog.
- *
- * @return The selected video file, or null if no file was selected.
- */
-fun selectVideo(): File? {
-    val chooser = JFileChooser()
-    return if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-        chooser.selectedFile
+fun openFileChooserAndGetPath(): String? {
+    val fileChooser = JFileChooser()
+    val result = fileChooser.showOpenDialog(null)
+    return if (result == JFileChooser.APPROVE_OPTION) {
+        fileChooser.selectedFile.absolutePath
     } else {
         null
     }
