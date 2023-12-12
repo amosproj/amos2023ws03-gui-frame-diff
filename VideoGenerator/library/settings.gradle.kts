9dbe7cd87159962b2d5190a7bb9eab558f593b38
Anton Kriese <anton.kriese@fu-berlin.de>
2023-12-12 12:57:10 +0100

    feat(lib1): back to proper gradle project
    adding gradlew scripts and a gradle settings file, one can
    now execute gradle tasks directly in the library module.
    
    Signed-off-by: Anton Kriese <anton.kriese@fu-berlin.de>

---
+++ VideoGenerator/library/settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        kotlin("jvm") version "1.9.0"
+    }
+}
+
+rootProject.name = "VideoGenerator"
