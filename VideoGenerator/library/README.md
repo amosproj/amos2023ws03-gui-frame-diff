cabb9e9d1878b5760a89e6e6a59bcdc95e4b85da
Anton Kriese <anton.kriese@fu-berlin.de>
2023-12-12 13:48:23 +0100

    doc(lib1): include license checking
    Signed-off-by: Anton Kriese <anton.kriese@fu-berlin.de>

--- VideoGenerator/library/README.md
+++ VideoGenerator/library/README.md
@@ -19,10 +19,16 @@ Before you get started, make sure you have the following installed:
 
 1. Clone the project repository to your local machine
 2. Change your working directory to the project folder
-3. Build the project using `gradle build`
-4. Run the project using `gradle run`
+3. Build the project using `./gradlew assemble`
+4. Execute unit tests using `./gradlew test`
 
 ### Initialization
 
 Your video generator should extend the `AbstractVideoGenerator` class. You can then initialize your video generator.
+
+### License check
+
+To generate a license report in `../licenses/VideoGenerator`, execute `./gradlew generateLicenseReport`.
+The gradle task `./gradlew checkLicense` can be used to check if any dependencies with disallowed licenses
+are in use. The tasks fails with an error if that is the case.
 
--- VideoGenerator/library/build.gradle.kts
+++ VideoGenerator/library/build.gradle.kts
@@ -1,6 +1,12 @@
+import com.github.jk1.license.filter.DependencyFilter
+import com.github.jk1.license.filter.LicenseBundleNormalizer
+import com.github.jk1.license.render.InventoryHtmlReportRenderer
+import com.github.jk1.license.render.ReportRenderer
+
 plugins {
     kotlin("jvm")
     application
+    id("com.github.jk1.dependency-license-report") version "2.5"
 }
 
 group = "org.example"
@@ -30,6 +36,12 @@ kotlin {
     jvmToolchain(11)
 }
 
+licenseReport {
+    outputDir = "../../licenses/reports/VideoGenerator"
+    renderers = arrayOf<ReportRenderer>(InventoryHtmlReportRenderer("report.html", "VideoGenerator"))
+    filters = arrayOf<DependencyFilter>(LicenseBundleNormalizer())
+    allowedLicensesFile = File("../../licenses/allowed-licenses.json")
+}
 /*
 // commented out, as this is now a library not containing a main function
 application {
