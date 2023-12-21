9891def1789110c96fdb13bce935a33e6bf077a5
a-miscellaneous <96189996+a-miscellaneous@users.noreply.github.com>
2023-12-21 11:41:59 +0100

    aded top bar with save collage button
    Signed-off-by: a-miscellaneous <96189996+a-miscellaneous@users.noreply.github.com>

--- GUI/src/main/kotlin/ui/screens/DiffScreen.kt
+++ GUI/src/main/kotlin/ui/screens/DiffScreen.kt
@@ -3,11 +3,15 @@ package ui.screens
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.input.key.*
+import androidx.compose.ui.unit.dp
 import frameNavigation.FrameNavigation
 import models.AppState
 import ui.components.*
@@ -38,6 +42,29 @@ fun DiffScreen(state: MutableState<AppState>) {
             focusRequester.requestFocus()
         }
 
+        TopAppBar {
+            Row(modifier = Modifier.fillMaxWidth()) {
+//                // #####   File Selectors   #####
+//                RowScope.FileSelectorButton(
+//                    buttonText = "Video 1",
+//                    buttonPath = state.value.video1Path,
+//                    onUpdateResult = { state.value.video1Path = it },
+//                )
+//                RowScope.FileSelectorButton(
+//                    buttonText = "Video 2",
+//                    buttonPath = state.value.video2Path,
+//                    onUpdateResult = { state.value.video2Path = it },
+//                )
+//                RowScope.FileSelectorButton(
+//                    buttonText = "Output",
+//                    buttonPath = state.value.outputPath,
+//                    onUpdateResult = { state.value.outputPath = it },
+//                )
+                // #####   Save Collage Button   #####
+                saveCollageButton(navigator)
+            }
+        }
+
         // #####   Titles   #####
         Row(modifier = Modifier.fillMaxWidth().weight(0.2f)) {
             textTitle(text = "Video 1")
@@ -58,4 +85,16 @@ fun DiffScreen(state: MutableState<AppState>) {
         // #####   Navigation   #####
         NavigationButtons(navigator, Modifier.weight(1f), Modifier.weight(0.15f))
     }
+}
+
+@Composable
+fun RowScope.saveCollageButton(navigator: FrameNavigation) {
+    // #####   Save Collage Button   #####
+    Button(
+        modifier = Modifier.weight(0.1f).padding(8.dp).fillMaxSize(),
+        onClick = { openFileChooserAndGetPath()?.let { navigator.createCollage(it) } },
+    ) {
+        Text(text = "Save Collage")
+    }
+    Spacer(modifier = Modifier.weight(0.9f))
 }
