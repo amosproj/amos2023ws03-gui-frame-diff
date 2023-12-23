e6c1b9f15f8e4184130efe5b3bf1d820307471b7
AlperK61 <92909013+AlperK61@users.noreply.github.com>
2023-12-23 08:28:46 +0100

    fix(gui): changed files appended
    Signed-off-by: AlperK61 <92909013+alperk61@users.noreply.github.com>
    Signed-off-by: AlperK61 <92909013+AlperK61@users.noreply.github.com>

--- GUI/src/main/kotlin/ui/screens/SettingsScreen.kt
+++ GUI/src/main/kotlin/ui/screens/SettingsScreen.kt
@@ -56,10 +56,36 @@ fun SettingsScreen(state: MutableState<AppState>) {
                 },
             )
         }
-        // save
         Row(modifier = Modifier.weight(0.2f)) {
+            // back
+            BackButton(state)
+            // save
             SaveButton(state)
         }
+    }
+}
+
+@Composable
+fun RowScope.BackButton(state: MutableState<AppState>) {
+    Button(
+        // fills all available space
+        modifier = Modifier.weight(0.1f).padding(8.dp).fillMaxSize(1f),
+        onClick = {
+            state.value =
+                // set all previus values
+                state.value.copy(
+                    screen = Screen.SelectVideoScreen,
+                    gapOpenPenalty = state.value.prevGapOpenPenalty,
+                    gapExtendPenalty = state.value.prevExtendPenalty,
+                    maskPath = state.value.prevMaskPath,
+                )
+        },
+    ) {
+        Image(
+            painter = painterResource("back-arrow.svg"),
+            contentDescription = "back",
+            modifier = Modifier.fillMaxSize().alpha(0.8f).padding(4.dp),
+        )
     }
 }
 
@@ -69,8 +95,13 @@ fun RowScope.SaveButton(state: MutableState<AppState>) {
         // fills all available space
         modifier = Modifier.weight(0.1f).padding(8.dp).fillMaxSize(1f),
         onClick = {
-            // set the screen
-            state.value = state.value.copy(screen = Screen.SelectVideoScreen)
+            state.value =
+                state.value.copy(
+                    screen = Screen.SelectVideoScreen,
+                    prevGapOpenPenalty = state.value.gapOpenPenalty,
+                    prevExtendPenalty = state.value.gapExtendPenalty,
+                    prevMaskPath = state.value.maskPath,
+                )
         },
     ) {
         Image(
