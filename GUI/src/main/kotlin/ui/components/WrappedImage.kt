d0a182639dcf5c76e9a108b6951bc7d94dd6b2f8
AlperK61 <92909013+AlperK61@users.noreply.github.com>
2023-12-26 23:42:22 +0100

    fix(gui): delete changes in WrappedImage.kt. Change AppState.kt and BackButton
    Signed-off-by: AlperK61 <92909013+alperk61@users.noreply.github.com>
    Signed-off-by: AlperK61 <92909013+AlperK61@users.noreply.github.com>

--- GUI/src/main/kotlin/ui/components/WrappedImage.kt
+++ GUI/src/main/kotlin/ui/components/WrappedImage.kt
@@ -6,17 +6,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.CursorDropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.isSecondaryPressed
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 
 /**
@@ -30,7 +25,6 @@ fun wrappedImage(
     bitmap: MutableState<ImageBitmap>,
     modifier: Modifier = Modifier,
 ) {
-    var expanded by remember { mutableStateOf(false) }
     Row(
         modifier =
             modifier.background(Color.Gray)
@@ -38,35 +32,5 @@ fun wrappedImage(
                 .fillMaxWidth(1f),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center,
-    ) {
-        Image(
-            bitmap = bitmap.value,
-            null,
-            modifier =
-                Modifier.pointerInput(Unit) {
-                    awaitPointerEventScope {
-                        while (true) {
-                            val event = awaitPointerEvent(PointerEventPass.Initial)
-                            if (event.changes.any { it.isConsumed }) {
-                                continue
-                            }
-
-                            if (event.buttons.isSecondaryPressed) {
-                                expanded = true
-                            }
-                        }
-                    }
-                },
-        )
-        CursorDropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false },
-        ) {
-            DropdownMenuItem({
-                println("'Save image as png' was clicked")
-            }) {
-                Text("Save image as png")
-            }
-        }
-    }
+    ) { Image(bitmap = bitmap.value, null) }
 }
--- GUI/src/main/kotlin/ui/screens/SettingsScreen.kt
+++ GUI/src/main/kotlin/ui/screens/SettingsScreen.kt
@@ -17,7 +17,11 @@ import ui.components.textTitle
  * SettingsScreen is the screen where the user can change the settings of the app.
  *
  * @param state the state of the app
+ * @param oldState the previous state of the app
  */
+
+var oldState = AppState()
+
 @Composable
 fun SettingsScreen(state: MutableState<AppState>) {
     // Contains the whole Screen
@@ -72,13 +76,7 @@ fun RowScope.BackButton(state: MutableState<AppState>) {
         modifier = Modifier.weight(0.1f).padding(8.dp).fillMaxSize(1f),
         onClick = {
             state.value =
-                // set all previus values
-                state.value.copy(
-                    screen = Screen.SelectVideoScreen,
-                    gapOpenPenalty = state.value.prevGapOpenPenalty,
-                    gapExtendPenalty = state.value.prevExtendPenalty,
-                    maskPath = state.value.prevMaskPath,
-                )
+                oldState.copy(screen = Screen.SelectVideoScreen)
         },
     ) {
         Image(
@@ -95,12 +93,10 @@ fun RowScope.SaveButton(state: MutableState<AppState>) {
         // fills all available space
         modifier = Modifier.weight(0.1f).padding(8.dp).fillMaxSize(1f),
         onClick = {
+            oldState = state.value
             state.value =
                 state.value.copy(
                     screen = Screen.SelectVideoScreen,
-                    prevGapOpenPenalty = state.value.gapOpenPenalty,
-                    prevExtendPenalty = state.value.gapExtendPenalty,
-                    prevMaskPath = state.value.maskPath,
                 )
         },
     ) {
