cb512fbb28e3f7f5bde4bf0d1234679e74a567a5
zino212 <luisguenther@gmx.de>
2024-01-26 16:55:06 +0100

    refactor(gui): Restyle LoadingDialog
    Signed-off-by: zino212 <luisguenther@gmx.de>

--- GUI/src/main/kotlin/ui/components/selectVideoScreen/LoadingDialog.kt
+++ GUI/src/main/kotlin/ui/components/selectVideoScreen/LoadingDialog.kt
@@ -6,12 +6,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -20,7 +19,14 @@ fun LoadingDialog(onCancel: () -> Unit = {}) {
     AlertDialog(
         modifier = Modifier.size(300.dp, 300.dp),
         onDismissRequest = onCancel,
-        title = { Text(text = "Computing") },
+        title = {
+            Text(
+                text = "Computing ...",
+                color = MaterialTheme.colorScheme.onSecondary,
+                fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                fontWeight = FontWeight.Bold,
+            )
+        },
         text = {
             Box(
                 contentAlignment = Alignment.Center,
@@ -34,8 +40,18 @@ fun LoadingDialog(onCancel: () -> Unit = {}) {
                 contentAlignment = Alignment.Center,
                 modifier = Modifier.fillMaxSize(),
             ) {
-                Button(onClick = onCancel) {
-                    Text("Cancel")
+                TextButton(
+                    onClick = { onCancel() },
+                    colors =
+                        ButtonDefaults.textButtonColors(
+                            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                            contentColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                        ),
+                ) {
+                    Text(
+                        text = "Cancel",
+                        color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    )
                 }
             }
         },
