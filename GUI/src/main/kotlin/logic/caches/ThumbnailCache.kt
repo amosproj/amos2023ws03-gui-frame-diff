a3c5f428fa1e50b0c83ae1d2087338a529a76174
Anton Kriese <anton.kriese@fu-berlin.de>
2024-02-03 17:28:40 +0100

    feat(gui): quit thumbnail job early if it is cancelled
    This is done by using yield() to check if the job is still
    running. If not, yield() throws a CancellationException
    which we can catch, returning the lock and no images.
    
    Signed-off-by: Anton Kriese <anton.kriese@fu-berlin.de>

--- GUI/src/main/kotlin/logic/caches/ThumbnailCache.kt
+++ GUI/src/main/kotlin/logic/caches/ThumbnailCache.kt
@@ -1,7 +1,9 @@
 package logic.caches
 
 import androidx.compose.ui.graphics.ImageBitmap
-import org.bytedeco.opencv.opencv_core.Mutex
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.yield
+import kotlin.coroutines.cancellation.CancellationException
 
 /**
  * A class that caches thumbnails for the timeline.
@@ -20,16 +22,32 @@ class ThumbnailCache(val maxCacheSize: Int, val getImages: (Int) -> List<ImageBi
      *
      * If the thumbnails are not already cached, they are loaded via the `getImages` function and cached.
      *
+     * The image retrieval is locked with a mutex to prevent race conditions. If the executing job is cancelled, while
+     * waiting for the lock, the function returns immediately.
+     *
      * @param index [Int] containing the index of the diff.
-     * @return [List]<[ImageBitmap]> containing the thumbnails for the given diff index.
+     * @return [List]<[ImageBitmap]> containing the thumbnails for the given diff index, optional.
      */
-    fun get(index: Int): List<ImageBitmap> {
+    suspend fun get(index: Int): List<ImageBitmap>? {
         recentlyUsed.remove(index)
-        println(index)
 
         if (!cache.containsKey(index)) {
+            // ask to enter the critical section, wait if it's already locked
             mutex.lock()
+
+            try {
+                // check if the images are still wanted
+                yield()
+            } catch (e: CancellationException) {
+                // ...if not, release the lock and return immediately
+                mutex.unlock()
+                return null
+            }
+
+            // actually grab the images
             cache[index] = getImages(index)
+
+            // release the lock as the critical section is over
             mutex.unlock()
         }
 
